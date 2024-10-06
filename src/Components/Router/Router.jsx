a072b4d91a60a70e09acee1342141862c3b25d7c
Toky <zosephatoky@gmail.com>
2024-10-06 17:56:50 +0300

    avant dernier

--- src/Components/Router/Router.jsx
+++ src/Components/Router/Router.jsx
@@ -3,6 +3,10 @@ import Index from '../Index/Index';
 import Login from '../Login/Login';
 import Dashbord from '../Admin/Dashbord/Dashbord';
 import PrivateRoute from './PrivateRoute';
+import $ from 'jquery';
+import 'perfect-scrollbar';
+window.jQuery = $;
+window.$ = $;
 
 
 import { useEffect } from 'react';
@@ -17,8 +21,10 @@ const Root = () => {
             import ('../../assets/front/css/material-kit.css?v=3.0.4');
         }else if(location.pathname.startsWith('/admin')){
             import ("../../assets/admin/css/bootstrap.min.css");
-            import ("../../assets/admin/css/now-ui-dashboard.min.css");
+            import ("../../assets/admin/js/now-ui-dashboard.js");
+            import ("../../assets/admin/css/now-ui-dashboard.css");
             import ("../../assets/admin/demo/demo.css");
+            import ("../../assets/admin/demo/demo.js");
         }else{
             import ('../../assets/front/css/nucleo-icons.css');
             import ('../../assets/front/css/nucleo-svg.css');
